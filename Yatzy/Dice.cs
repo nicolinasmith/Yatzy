f3fe0dad5bb9d3353269e5c1a1c326dd806efd54
Nicolina Smith <113508253+nism07@users.noreply.github.com>
2023-11-29 18:25:09 +0100

    Add project files.

---
+++ Yatzy/Dice.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yatzy
+{
+    public class Dice
+    {
+        public int Value { get; set; }
+
+        public bool IsChosen { get; set; }
+
+    }
+}
---
+++ Yatzy/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Yatzy
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        private Dice[] dices = new Dice[4];
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            for (int i = 0; i < dices.Length; i++)
+            {
+                dices[i] = new Dice();
+            }
+        }
+
+
+        private void RollDice_Click(object sender, RoutedEventArgs e)
+        {
+            Random random = new Random();
+
+            dices[0].Value = 1;
+            dices[1].Value = 2;
+            dices[2].Value = 3;
+
+
+        }
+    }
+
+
+}
